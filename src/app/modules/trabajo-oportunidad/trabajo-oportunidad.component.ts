1153e1ca893bf662172a3b56fd65f3610611bf33
brayanmedinac <64040082+brayanmedinac@users.noreply.github.com>
2020-05-21 10:54:38 -0500

    campos formulario trabajo oportunidad

--- src/app/modules/trabajo-oportunidad/trabajo-oportunidad.component.ts
+++ src/app/modules/trabajo-oportunidad/trabajo-oportunidad.component.ts
@@ -9,7 +9,67 @@ export class TrabajoOportunidadComponent implements OnInit {
 
   constructor() { }
 
+  panelOpenState = false;
+  customCollapsedHeight: string = '25px';
+  customExpandedHeight: string = '25px';
+
+  form = {
+    numeroConsigna:{
+      label: 'Consignación No.',
+      name: 'numeroConsigna',
+      value: null,
+      messages: null,
+      required: false,
+    },
+    trabajo:{
+      label: 'Trabajos',
+      name: 'Trabajos',
+      value: null,
+      messages: null,
+      required: false,
+    },
+    medidaSeguridad:{
+      label: 'Medidas de seguridad',
+      name: 'medidaSeguridad',
+      value: null,
+      messages: null,
+      required: false,
+    },
+    jefeTrabajo:{
+      label: 'Jefe de trabajos',
+      name: 'jefeTrabajo',
+      value: null,
+      messages: null,
+      required: false,
+    },
+    telefono:{
+      label: 'Telefono',
+      name: 'telefono',
+      value: null,
+      messages: null,
+      required: false,
+    },
+    elemnto: {
+      label: 'Elemento',
+      name: 'elemnto',
+      value: null,
+      messages: null,
+      required: false,
+    }
+  }
+
+  dataControls = {
+    elemento:[
+      {nombre:'xxxxxx',codigo:'x'},
+      {nombre:'xxxxx',codigo:'x'},
+    ]
+  }
+
   ngOnInit(): void {
+  }
+
+  setData(name, event) {
+    this.form[name].value = event;
   }
 
 }
--- src/app/modules/trabajo-oportunidad/trabajo-oportunidad.module.ts
+++ src/app/modules/trabajo-oportunidad/trabajo-oportunidad.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrabajoOportunidadComponent } from './trabajo-oportunidad.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {CustomFormsModule} from '../../ui/forms/custom-forms.module';
+import {MatSelectModule} from '@angular/material/select';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [TrabajoOportunidadComponent],
   imports: [
     CommonModule,
-    
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule,
+    FormsModule,
+    CustomFormsModule,
+    MatSelectModule,
+    MatExpansionModule,
   ]
 })
 export class TrabajoOportunidadModule { }
