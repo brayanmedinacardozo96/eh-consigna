0e98e03bc14313cd18fe7f6f2ad310a7bd19fa7e
Jhonatan Parra Almario <jhonatanjopa@gmail.com>
2020-05-26 09:23:19 -0500

    tabla en lista de elementos

--- src/app/modules/consigna/consigna-elemento-list/consigna-elemento-list.component.ts
+++ src/app/modules/consigna/consigna-elemento-list/consigna-elemento-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-consigna-elemento-list',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsignaElementoListComponent implements OnInit {
 
+  displayedColumns: string[] = ['numeroConsigna', 'tipoZona', 'estadoConsigna', 'elementosConsignados', 'trabajosOportunidad', 'maniobras', 'acciones'];
+  dataSource: MatTableDataSource<any>;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
   constructor() { }
+
+  @Input() set data(data: []) {
+    this.init(data);
+  }
 
   ngOnInit(): void {
   }
 
+  init(data) {
+    this.dataSource = new MatTableDataSource(data);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 }
