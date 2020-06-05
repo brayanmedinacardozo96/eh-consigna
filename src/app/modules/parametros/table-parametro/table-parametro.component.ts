a780ebb7613fb2884be4eaf70258129d8887d077
brayanmedinac <64040082+brayanmedinac@users.noreply.github.com>
2020-06-04 22:01:21 -0500

    color

---
+++ src/app/modules/parametros/table-parametro/table-parametro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-parametro',
+  templateUrl: './table-parametro.component.html',
+  styleUrls: ['./table-parametro.component.scss']
+})
+export class TableParametroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/ui/forms/input-text/input-text.component.ts
+++ src/app/ui/forms/input-text/input-text.component.ts
@@ -14,6 +14,7 @@ export class InputTextComponent implements OnInit {
   @Input() style;
   @Input() disable;
   @Input() required;
+  @Input() length;
   @Output() valueChange = new EventEmitter();
   constructor() { }
 
