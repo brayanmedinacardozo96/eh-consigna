cdf26fbf18fdbac3cf43fbfc8f055541a8871f0c
Jhonatan Parra Almario <jhonatanjopa@gmail.com>
2020-06-10 15:43:21 -0500

    proceso de actualización, falta agregar formularios resgistro de maniobra y trabajos de oportunidad.

--- src/app/modules/consigna/consigna-user/consigna-user.component.ts
+++ src/app/modules/consigna/consigna-user/consigna-user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from '../../../../environments/environment';
+import { ApiService } from './../../../shared/services/api.service';
+import { User } from './../../../shared/models/user';
+import { Auth } from './../../../shared/auth';
+import { SnackBarService } from './../../../shared/services/snack-bar.service';
 
 @Component({
   selector: 'app-consigna-user',
@@ -7,6 +12,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsignaUserComponent implements OnInit {
 
+  user: User = Auth.getUserDataPerson();
+  request = {
+    solicitante: {value: this.user.id},
+    codigoSolicitada: ['S'],
+    codigoAprobEjecu: ['A','E'],
+    codigoCancelRepro: ['C','R']
+  };
+  total = {
+    totalPendientes: 0,
+    totalAprobEjecu: 0,
+    totalCancelRepro: 0
+  };
   data = [
     {numeroConsigna:'1234',tipoZona:'ZN',estadoConsigna:'Pendiente',
       elementosConsignados:[
@@ -45,15 +62,38 @@ export class ConsignaUserComponent implements OnInit {
 
   viewList = false;
 
-  constructor() {
+  constructor(private api: ApiService,
+              private snackBar: SnackBarService) {
     this.viewList = false;
    }
 
   ngOnInit(): void {
+    this.search();
   }
 
-  buscarConsigna(){
+  async buscarConsigna(codigoEstado){
     this.viewList = true;
+    let params = {
+      solicitante:{value: this.user.id},
+      codigoEstadoConsigna:{value:codigoEstado}
+    }
+    const response = await this.api.post(`${environment.apiBackend}/consigna/get-list`, params);
+    if(response.success){
+      this.data = response.data;
+      if(this.data.length < 1){
+        this.snackBar.alert('No se encontraron registros con los parámetros consultados.',5000);
+      }
+    }
   }
 
+  async search() {
+    this.data = [];
+    const response = await this.api.post(`${environment.apiBackend}/consigna/get-my-consigna`, this.request);
+    if(response.success){
+      let data = response.data;
+      this.total.totalPendientes = data.totalPendientes;
+      this.total.totalAprobEjecu = data.totalAprobEjecu;
+      this.total.totalCancelRepro = data.totalCancelRepro;
+    }
+  }
 }
