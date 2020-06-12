d3c68bc6a6fbf1783662c46a9b09e5ff5c2127af
Jhonatan Parra Almario <jhonatanjopa@gmail.com>
2020-06-12 08:46:11 -0500

    change enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   companyName: 'Electrohuila S.A. - E.S.P.',
   apiTransverseSecurity: 'http://192.9.200.44/back-transverse-security/public/api',
   keyTransverseSecurity: '$2y$10$OjsZGz0JqaUiFvvLHBt1I.sv5oW3bcZTOTavMOqwfAT2TxYhZa1HS',
-  apiBackend: 'http://192.9.200.44/eh-consigna-backend/public/api',
-  urlFiles: 'http://192.9.200.44/eh-consigna-backend/',
+  apiBackend: 'http://192.9.200.44/back-consignas/public/api',
+  urlFiles: 'http://192.9.200.44/back-consignas/',
 };
