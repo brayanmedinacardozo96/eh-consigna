bc48e364b4281d584a67835f1e9750142c96853f
Juan Sebastian Villegas Conde <jusevico@gmail.com>
2020-07-16 18:28:21 -0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import {LoginComponent} from './modules/login/login.component';
 import {DashboardComponent} from './modules/dashboard/dashboard.component';
 import {AuthGuardService as AuthGuard} from './shared/auth-guard.service';
 import {NotFoundComponent} from './ui/not-found/not-found.component';
-import {GenerateInvoicesComponent} from './modules/generate-invoices/generate-invoices.component';
-import {HistoryInvoicesComponent} from './modules/history-invoices/history-invoices.component';
 import {ConsignaComponent} from './modules/consigna/consigna.component';
 import {TrabajoOportunidadComponent} from './modules/trabajo-oportunidad/trabajo-oportunidad.component';
 import {ManiobraComponent} from './modules/maniobra/maniobra.component';
@@ -16,14 +14,12 @@ import {ParametrosComponent} from './modules/parametros/parametros.component';
 import {ComunicadoPrensaFormComponent} from "./modules/comunicado-prensa/comunicado-prensa-form/comunicado-prensa-form.component";
 import {ComunicadoPrensaComponent} from "./modules/comunicado-prensa/comunicado-prensa.component";
 import {SeguimientoConsignaComponent} from './modules/consigna/seguimiento-consigna/seguimiento-consigna.component';
+import {BitacoraFormComponent} from "./modules/bitacora/bitacora-form/bitacora-form.component";
 const routes: Routes = [
   {path: '', component: LoginComponent}
   , {path: 'login', component: LoginComponent}
 
   , {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]}
-  , {path: 'generate-invoices', component: GenerateInvoicesComponent, canActivate: [AuthGuard]}
-  , {path: 'history-invoices', component: HistoryInvoicesComponent, canActivate: [AuthGuard]}
-  , {path: 'history-invoices/:package', component: HistoryInvoicesComponent, canActivate: [AuthGuard]}
   , {path: 'consigna', component: ConsignaComponent, canActivate: [AuthGuard]}
   , {path: 'mis-consignas', component: ConsignaUserComponent, canActivate: [AuthGuard]}
   , {path: 'consigna/nueva-consigna', component: ConsignaTabsComponent, canActivate: [AuthGuard]}
@@ -33,11 +29,11 @@ const routes: Routes = [
   , {path: 'autorizar/:id', component: AutorizarComponent, canActivate: [AuthGuard]}
   , {path: 'autorizar', component: AutorizarComponent, canActivate: [AuthGuard]}
   , {path: 'parametros', component: ParametrosComponent, canActivate: [AuthGuard]}
-
   , {path: 'comunicado-prensa', component: ComunicadoPrensaComponent, canActivate: [AuthGuard]}
   , {path: 'comunicado-prensa/new', component: ComunicadoPrensaFormComponent, canActivate: [AuthGuard]}
   , {path: 'comunicado-prensa/edit/:id', component: ComunicadoPrensaFormComponent, canActivate: [AuthGuard]}
   , {path: 'seguimiento-consigna', component: SeguimientoConsignaComponent, canActivate: [AuthGuard]}
+  , {path: 'bitacora/new', component: BitacoraFormComponent, canActivate: [AuthGuard]}
 
   , {path: '404', component: NotFoundComponent}
   , {path: '**', redirectTo: '/404'}
