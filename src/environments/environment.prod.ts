405b43f539a335e7f1199c76021d10531a34237d
Jhonatan Parra Almario <jhonatanjopa@gmail.com>
2020-07-28 13:49:52 -0500

    modificado enviroment apuntando al backend en producción.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,9 +4,9 @@ export const environment = {
   companyName: 'Electrohuila S.A. - E.S.P.',
   apiTransverseSecurity: 'http://192.9.200.44/back-transverse-security/public/api',
   keyTransverseSecurity: '$2y$10$OjsZGz0JqaUiFvvLHBt1I.sv5oW3bcZTOTavMOqwfAT2TxYhZa1HS',
-  apiBackend: 'http://192.9.200.44/back-consignas/public/api',
-  urlFiles: 'http://192.9.200.44/back-consignas/',
-  urlPublicFiles: 'http://192.9.200.44/back-consignas/public/',
+  apiBackend: 'http://200.21.4.67/back-consignas/public/api',
+  urlFiles: 'http://200.21.4.67/back-consignas/',
+  urlPublicFiles: 'http://200.21.4.67/back-consignas/public/',
   urlFrontendST: 'https://enlinea.electrohuila.com.co/transverse-security/#',
   urlEhmap:'https://enlinea.electrohuila.com.co/ehmaps/mapa.php?mapa=2&h=1',
 };
