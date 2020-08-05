23442d1b40f2897cf31f1774bffd28ab0c3e0211
Jhonatan Parra Almario <jhonatanjopa@gmail.com>
2020-08-05 17:43:34 -0500

    modificado el componente input-number para solo adminir números y corrección de issues encontrados.

--- src/app/ui/forms/input-number/input-number.component.ts
+++ src/app/ui/forms/input-number/input-number.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-input-number',
@@ -7,11 +8,13 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 })
 export class InputNumberComponent implements OnInit {
 
+  @Input() id;
   @Input() label;
   @Input() value;
   @Input() name;
   @Input() messages;
   @Input() style;
+  @Input() required;
   @Output() valueChange = new EventEmitter();
 
   constructor() {
@@ -20,4 +23,18 @@ export class InputNumberComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  checkPattern(event){    
+    var re = /^[0-9]+$/;
+    var text = '';
+
+    for(let value in event){
+      if(re.test(event[value])){
+        text += event[value];
+      }
+    }
+
+    this.value = text;
+    $('#'+this.id).val(this.value);
+    this.valueChange.emit(this.value);
+  }
 }
