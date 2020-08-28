632ad6ddbfa3ea1f4e4b9c4c2fbde672dea6da7e
brayanmedinac <64040082+brayanmedinac@users.noreply.github.com>
2020-08-28 07:08:36 -0500

    fecha

--- src/app/modules/consigna/consigna-list/consigna-list.component.ts
+++ src/app/modules/consigna/consigna-list/consigna-list.component.ts
@@ -23,7 +23,7 @@ import {Aprobar} from '../../autorizar/aprobar';
 export class ConsignaListComponent implements OnInit {
   number = Number;
 
-  displayedColumns: string[] = ['numeroConsigna', 'consecutivoSnc','fecha_solicitud', 'tipoFormato', 'tipoZona', 'estadoConsigna','estadoEquipo', 'tipo_consignacion', 'solicitante', 'consignaPadre', 'elementosConsignados', 'maniobras', 'acciones'];
+  displayedColumns: string[] = ['numeroConsigna', 'consecutivoSnc','fecha_creacion','fecha_solicitud', 'tipoFormato', 'tipoZona', 'estadoConsigna','estadoEquipo', 'tipo_consignacion', 'solicitante', 'consignaPadre', 'elementosConsignados', 'maniobras', 'acciones'];
   dataSource: MatTableDataSource<any>;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
