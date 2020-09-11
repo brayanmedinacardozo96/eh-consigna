eb0541c44a4ba4a1620d7fb132f693ac0d2ce463
Jhonatan Parra Almario <jhonatanjopa@gmail.com>
2020-09-10 22:43:18 -0500

    implementado para poder obtener las consignas deacuerdo al consolidado

--- src/app/modules/consolidado/consolidado-list/consolidado-list.component.ts
+++ src/app/modules/consolidado/consolidado-list/consolidado-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ConsolidadoConsignaListComponent } from './../consolidado-consigna-list/consolidado-consigna-list.component';
 import { ConfirmDialogComponent, ConfirmDialogModel } from './../../../ui/confirm-dialog/confirm-dialog.component';
 import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
 import {environment} from "../../../../environments/environment";
@@ -21,6 +22,24 @@ export class ConsolidadoListComponent implements OnInit {
     private dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  async openVerConsignas(id){
+    let params = {id:id}
+    let consignas = []
+    const response = await this.api.post(`${environment.apiBackend}/consolidado-prensa/get-consignacion-list`,params);
+    console.log(response.data)
+    if(response.success){
+      for(let value of response.data){
+        for(let consigna of value.consignaciones){
+          consignas.push(consigna)
+        }
+      }
+      const dialogRef = this.dialogConfirm.open(ConsolidadoConsignaListComponent,{
+        width:'100%',
+        data: {consignas}
+      });
+    }
   }
 
 }
